ba08a695cedba36be2647c2b10d2150b751e9286
x <bjmorse@gmail.com>
2020-01-01 04:14:46 -0800

    getting tricky with renders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ShallowRenderer from 'react-test-renderer/shallow'; // ES6
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      count: 0
+      display_count: 0
+      ,count:0
     };
 
     this.handleStop = this.handleStop.bind(this);
@@ -30,16 +33,52 @@ class App extends React.Component {
 
   tick() {
     const s = this.state;
+
+    // console.log('A: ' + s.count);
+
     this.setState({
       count: s.count+1
     });
+    // console.log('B: ' + s.count);
+
+    if( this.isMacroUpdate(s) ) {
+      this.setState({
+        display_count: s.count
+      });
+    } else {
+      if( !this.cachedId ) {
+        this.cachedId = document.getElementById('fastCount');
+      }
+    }
+
+
+
+
+    // this.setState({
+    //   display_count: s.display_count+1
+    // });
+  }
+
+  isMacroUpdate(s) {
+    if((s.count % 10) === 0) {
+      return true;
+    }
+    return false;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if( this.isMacroUpdate(nextState) ) {
+      return true;
+    }
+    return false;
   }
 
   render() {
+    console.log(this.props);
     let s = this.state;
     return (
       <div>
-        <h1>{s.count}</h1>
+        <h1><div id="fastCount">{s.display_count}</div></h1>
         <button onClick={this.handleStop}>Stop Counter</button><br/>
       </div>
     );
